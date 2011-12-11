a2a32c459cc91154b1da9c1e07f7846716131935
Rob Apodaca <rob.apodaca@gmail.com>
2011-12-11 16:43:36 -0600

    Placed Property tests into correct test file

--- tests/PropertyTest.php
+++ tests/PropertyTest.php
@@ -1,33 +1,39 @@
 <?php
-require_once 'PHPUnit/Framework.php';
-require_once 'Mockery/Framework.php';
-include_once(dirname(__FILE__) . '/../lib/pillow/Base.php');
-include_once(dirname(__FILE__) . '/../lib/pillow/Property.php');
+/**
+ * @author Rob Apodaca <rob.apodaca@gmail.com>
+ * @copyright Copyright (c) 2011, Rob Apodaca
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
 
+require __DIR__ . '/../lib/Autoloader.php';
+
+use Pillow\Property;
 
 class PropertyTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetMethods()
-    {
-        $zestimate = mockery('pz', 'Pillow_Zestimate');
-        $chart = mockery('pc', 'Pillow_Chart');
-
-        $factory = mockery('Pillow_Factory', array(
-            'createZestimate' => $zestimate,
-            'createChart' => $chart
-        ));
-
-        $property = new Pillow_Property;
-        $property->setFactory($factory);
-
-        $ze = $property->getZestimate();
-        $this->assertSame($zestimate, $ze);
-
-        $ch = $property->getChart(Pillow_Property::CHART_UNIT_DOLLAR);
-        $this->assertSame($chart, $ch);
-        
-    }
- 
+  /**
+   * createFromXml
+   * 
+   * @test
+   */
+  public function mapsCorrectly() {
+    $xml = simplexml_load_file(__DIR__ .'/responses/search_results.xml');
+    $prop = Property::createFromXml($xml);
+    
+    $this->assertEquals('48749425', $prop->zpid);
+    
+    $this->assertEquals('2114 Bigelow Ave N', $prop->street);
+    $this->assertEquals('98109', $prop->zipcode);
+    $this->assertEquals('Seattle', $prop->city);
+    $this->assertEquals('WA', $prop->state);
+    $this->assertEquals('47.63793', $prop->latitude);
+    $this->assertEquals('-122.347936', $prop->longitude);
+    
+    $this->assertInstanceOf('\Pillow\Links', $prop->links);
+    $this->assertEquals("http://www.zillow.com/homedetails/2114-Bigelow-Ave-N-Seattle-WA-98109/48749425_zpid/", $prop->links->homedetails);
+    $this->assertEquals('http://www.zillow.com/homedetails/charts/48749425_zpid,1year_chartDuration/?cbt=7522682882544325802%7E9%7EY2EzX18jtvYTCel5PgJtPY1pmDDLxGDZXzsfRy49lJvCnZ4bh7Fi9w**', $prop->links->graphsanddata);
+    $this->assertEquals('http://www.zillow.com/homes/map/48749425_zpid/', $prop->links->mapthishome);
+    $this->assertEquals('http://www.zillow.com/myestimator/Edit.htm?zprop=48749425', $prop->links->myestimator);
+    $this->assertEquals('http://www.zillow.com/homes/comps/48749425_zpid/', $prop->links->comparables);
+  }
 }
-
-?>
--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -33,32 +33,7 @@ class ServiceTest extends PHPUnit_Framework_TestCase
   public function createResultSetWithOneResult() {
     $results = $this->service->getSearchResults($this->address, $this->zip);
     
-    $this->assertSame(1, count($results));
-  }
-  
-  /**
-   * getSearchResults
-   * 
-   * @test
-   */
-  public function resultIsMappedCorrectly() {
-    $results = $this->service->getSearchResults($this->address, $this->zip);
-    $prop = $results->current();
-    
-    $this->assertEquals('48749425', $prop->zpid);
-    
-    $this->assertEquals('2114 Bigelow Ave N', $prop->street);
-    $this->assertEquals('98109', $prop->zipcode);
-    $this->assertEquals('Seattle', $prop->city);
-    $this->assertEquals('WA', $prop->state);
-    $this->assertEquals('47.63793', $prop->latitude);
-    $this->assertEquals('-122.347936', $prop->longitude);
-    
-    $this->assertInstanceOf('\Pillow\Links', $prop->links);
-    $this->assertEquals("http://www.zillow.com/homedetails/2114-Bigelow-Ave-N-Seattle-WA-98109/48749425_zpid/", $prop->links->homedetails);
-    $this->assertEquals('http://www.zillow.com/homedetails/charts/48749425_zpid,1year_chartDuration/?cbt=7522682882544325802%7E9%7EY2EzX18jtvYTCel5PgJtPY1pmDDLxGDZXzsfRy49lJvCnZ4bh7Fi9w**', $prop->links->graphsanddata);
-    $this->assertEquals('http://www.zillow.com/homes/map/48749425_zpid/', $prop->links->mapthishome);
-    $this->assertEquals('http://www.zillow.com/myestimator/Edit.htm?zprop=48749425', $prop->links->myestimator);
-    $this->assertEquals('http://www.zillow.com/homes/comps/48749425_zpid/', $prop->links->comparables);
+    $this->assertEquals(1, count($results));
+    $this->assertInstanceOf('\Pillow\Property', $results->current());
   }
 }
