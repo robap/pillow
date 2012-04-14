5ca59f47b20a67ad65f623f1e311f289dc685e75
Rob Apodaca <rob.apodaca@gmail.com>
2012-04-14 10:41:48 -0500

    end of life

--- lib/pillow.php
+++
@@ -1,8 +0,0 @@
-<?php
-require dirname( __FILE__ ) . '/pillow/Transporter.php';
-require dirname( __FILE__ ) . '/pillow/Base.php';
-require dirname( __FILE__ ) . '/pillow/Property.php';
-require dirname( __FILE__ ) . '/pillow/Zestimate.php';
-require dirname( __FILE__ ) . '/pillow/Chart.php';
-require dirname( __FILE__ ) . '/pillow/Factory.php';
-?>
